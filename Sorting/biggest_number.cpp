5109e5b7b4057e1f36e4582dcb9592c15a3b18ba
LeeChungHyun <38039844+LeeChungHyun@users.noreply.github.com>
2020-03-09 13:48:55 +0900

    Update biggest_number.cpp

--- Sorting/biggest_number.cpp
+++ Sorting/biggest_number.cpp
@@ -8,21 +8,21 @@ using namespace std;
 bool compare(string a, string b){
     if (a+b > b+a) return true;
     else return false;
-}
+}//각 문자끼리 합쳐서 비교해서 큰 수으로 정렬한다.
 
 string solution(vector<int> numbers) {
     string answer = "";
     vector<string>array;
     for(int i=0; i<numbers.size(); i++){
         array.push_back(to_string(numbers[i]));
-    }
-    sort(array.begin(), array.end(), compare);
+    }//string으로 바꾸고 array벡터에 넣는다.
+    sort(array.begin(), array.end(), compare);//오름차순으로 정렬한다.
 
     for(int i=0; i < array.size(); i++){
         answer = answer + array[i];
     }
 
-    if(answer[0] == '0') answer = '0';
+    if(answer[0] == '0') answer = '0';//0이면 0반환
 
     return answer;
 }
